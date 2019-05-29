d6bc04fab6d2c76c555c74af372b02c4243be969
Aspirate Qiu <jousindea@qq.com>
2019-05-29 22:09:19 +0800

    没有头绪的提交

--- nodeJsTest/routes/dict/MicrosoftDict.js
+++ nodeJsTest/routes/dict/MicrosoftDict.js
@@ -1,6 +1,23 @@
 var http = require("https");
 var cheerio = require("cheerio");
 var fs = require("fs");
+var mysql = require("mysql");
+
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '121934+1031Jou',
+    database : 'test'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
+
 
 var url = "https://cn.bing.com/dict/search?q=char&FORM=BDVSP6&mkt=zh-cn";
 var strHtml = "";
@@ -11,7 +28,40 @@ http.get(url, function (res) {
     });
     res.on("end", function () {
         var $ = cheerio.load(strHtml);
-        // console.log(strHtml);
-        console.log($("body *").text());
-    })
+        // 获取词义区块
+        var description = $(".qdef");
+        // 获取单词名称
+        var getVerb = description.find("h1").text();
+        // console.log(getVerb);
+        // 获取单词发音 yinBiao
+        var yinBiao = {
+            uk: description.find("[class='hd_pr']").text(),
+            us: description.find("[class='hd_prUS']").text(),
+        };
+        // 获取大概词义列表 liList
+        function getVerbDes (obj) {
+            var liList = {};
+            obj.find("ul .pos").each(function (index) {
+                var ciXing = $(this).text();
+                var ciYi = $(this).next(".def").text();
+                liList[ciXing] = ciYi;
+            });
+            // console.log(liList);
+            return liList;
+        }
+        // console.log(yinBiao);
+        var verb_des = {
+            "getVerb": getVerb,
+            "getCI": getVerbDes(description),
+        };
+        console.log(verb_des);
+        // '${newsTitle}'
+        connection.query(`creat`)
+        connection.query(`INSERT INTO data (mingzi, ciyi) VALUE('${verb_des.getVerb}', '${verb_des.getCI["n."]}')`,function(err,data) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
+
 });
--- nodeJsTest/routes/studyDemo之nodejs权威指南/6-2.以同步方式读取文件.js
+++ nodeJsTest/routes/studyDemo之nodejs权威指南/6-2.以同步方式读取文件.js
@@ -4,8 +4,18 @@ try {
     // 当直接使用相对路径"./fstext.txt"时，出现错误找不到文件
     // 此时使用path
     var filePath = path.join(__dirname, "./fstext.txt");
-    var data = fs.readFileSync(filePath, {encoding: "utf-8", flag: "r"});
-    console.log(data);
+    var data = fs.readFileSync(filePath, {encoding: "utf-8", flag: "r",});
+    var FileEveryLine = data.split("\r\n");
+    var FileLength = FileEveryLine.length;
+    var saveFileNotNone = [];
+    FileEveryLine.forEach(function (value, index, array) {
+        var trimStr = value.trim();
+       if (trimStr !== "") {
+           console.log(value, index);
+           saveFileNotNone.push(trimStr);
+       }
+    });
+    console.log(saveFileNotNone);
 }
 catch(err) {
     console.log(err);
