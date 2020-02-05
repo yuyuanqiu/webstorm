304863af9e0f2c08412b5112db9c3a53a6955ee4
yuyuanqiu <jousindea@163.com>
2020-02-05 16:05:16 +0800

    update vue study

---
+++ vue-study-project/05工具/01单文件组件hello.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>{{ greeting }} world!</p>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {
+            greeting: 'hello'
+        }
+    }
+}
+</script>
+
+<style scoped>
+p {
+    font-size: 2em;
+    text-align: center;
+}
+</style>
---
+++ vue-study-project/05工具/02单文件组件hello.vue
@@ -0,0 +1,27 @@
+
+// 使用预处理器
+<template lang="jade">
+div
+    <p>{{ greeting }} world!</p>
+    other-component
+</template>
+
+<script>
+import OtherComponent from './01单文件组件hello'
+module.exports = {
+    data() {
+        return {
+            greeting: 'hello'
+        }
+    },
+    components: {
+        OtherComponent
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+p
+    font-size 2em
+    text-align  center
+</style>
